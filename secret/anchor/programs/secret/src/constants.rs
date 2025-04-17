c19baa05abb9930b26e6f8e9e65371f59cbf956f
ulieth <uliana.eth@proton.me>
2025-04-17 21:55:21 +0300

    constants to derive size for Anchor

---
+++ secret/anchor/programs/secret/src/constants.rs
@@ -0,0 +1,7 @@
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+pub const USERNAME_MAX_LEN: usize = 30;
+pub const BIO_MAX_LEN: usize = 280; // Twiter style
+pub const INTERESTS_MAX_LEN: usize = 100;
+pub const GENDER_MAX_LEN: usize = 20;
+pub const LOOKING_FOR_MAX_LEN: usize = 20;
+pub const AVATAR_URI_MAX_LEN: usize = 204;
