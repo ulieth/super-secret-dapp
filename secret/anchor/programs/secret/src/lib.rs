5fb27cc4ee025bd83ce244fcbddcd8cd18e9144e
ulieth <uliana.eth@proton.me>
2025-04-18 21:08:52 +0300

    added two instructions: create profile and update profile bio

--- secret/anchor/programs/secret/src/lib.rs
+++ secret/anchor/programs/secret/src/lib.rs
@@ -2,48 +2,133 @@
 
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::system_instruction;
+
 pub mod constants;
+pub mod errors;
+pub mod events;
 pub use constants::*;
+pub use errors::*;
+pub use events::*;
 
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
 #[program]
 pub mod secret {
     use super::*;
-    pub fn initialize_profile(
-      ctx: Context<InitializeProfile>,
-      username: String,
-      bio: String,
-      gender: String,
-      interests: String,
-      looking_for: String,
-      avatar_uri: String,
+    pub fn create_profile(
+        ctx: Context<CreateProfile>,
+        profile_name: String,
+        bio: String,
+        gender: String,
+        looking_for: String,
+        avatar_uri: String,
     ) -> Result<()> {
+        // Validate input lengths
+        require!(
+            profile_name.len() <= PROFILE_NAME_MAX_LEN,
+            CustomError::InvalidUsernameLength
+        );
+        require!(
+            bio.len() <= BIO_MAX_LEN,
+            CustomError::InvalidBioLength
+        );
+        require!(
+            gender.len() <= GENDER_MAX_LEN,
+            CustomError::InvalidGenderLength
+        );
+        require!(
+            looking_for.len() <= LOOKING_FOR_MAX_LEN,
+            CustomError::InvalidLookingForLength
+        );
+        require!(
+            avatar_uri.len() <= AVATAR_URI_MAX_LEN,
+            CustomError::InvalidAvatarURILength
+        );
 
+        let current_time = Clock::get()?.unix_timestamp;
 
+        *ctx.accounts.profile = Profile {
+            authority: ctx.accounts.authority.key(),
+            profile_name,
+            bio,
+            gender,
+            looking_for,
+            avatar_uri,
+            created_at: current_time,
+            updated_at: current_time,
+            paused: false,
+            deleted_at: None,
+            vault_bump: ctx.bumps.vault,
+        };
 
-      let current_time = Clock::get()?.unix_timestamp;
+        // Ensure the vault PDA is owned by the program
+        require!(ctx.accounts.vault.owner == ctx.program_id, CustomError::Unauthorized);
 
-      // Ensure the vault PDA is owned by the program
-      require_keys_eq!(ctx.accounts.vault.owner, ctx.program_id, CustomError::Unauthorized);
+        emit!(CreateProfileEvent {
+            profile_key: ctx.accounts.profile.key(),
+            profile_name: ctx.accounts.profile.profile_name.clone(),
+            bio: ctx.accounts.profile.bio.clone(),
+            gender: ctx.accounts.profile.gender.clone(),
+            looking_for: ctx.accounts.profile.looking_for.clone(),
+            created_at: current_time,
+        });
 
-      Ok(())
+        Ok(())
     }
+
+    pub fn update_profile_bio(
+        ctx: Context<UpdateProfile>,
+        bio: String,
+    ) -> Result<()> {
+        let profile = &mut ctx.accounts.profile;
+        let current_time = Clock::get()?.unix_timestamp;
+
+        require!(
+            !bio.eq(&profile.bio),
+            CustomError::AlreadyUpdated
+        );
+        require!(
+            bio.len() <= BIO_MAX_LEN,
+            CustomError::InvalidBioLength
+        );
+
+        profile.bio = bio;
+
+        emit!(UpdateProfileEvent {
+            profile_key: profile.key(),
+            profile_name: profile.profile_name.clone(),
+            bio: profile.bio.clone(),
+            updated_at: current_time,
+        });
+
+        Ok(())
+    }
+
+
 }
 
 #[derive(Accounts)]
-pub struct InitializeProfile<'info> {
+#[instruction(
+    profile_name: String,
+    bio: String,
+    gender: String,
+    looking_for: String,
+    avatar_uri: String
+)]
+pub struct CreateProfile<'info> {
     #[account(mut)]
     pub authority: Signer<'info>,
+
     #[account(
         init,
         payer = authority,
-        space = ANCHOR_DISCRIMINATOR_SIZE + User::INIT_SPACE,
-        seeds = [b"user", authority.key().as_ref()],
+        space = ANCHOR_DISCRIMINATOR_SIZE + Profile::INIT_SPACE,
+        seeds = [b"profile", authority.key().as_ref()],
         bump
     )]
-    user: Account<'info, User>,
-    /// The vault that will hold SOL for each received like.
+    profile: Account<'info, Profile>,
+
+    /// The vault that will hold SOL for each received super like.
     /// Why we use a separate vault:
     /// - Solana accounts must remain **rent-exempt** to persist.
     /// - If we store lamports directly in the `User` account and its balance drops below
@@ -68,86 +153,50 @@ pub struct InitializeProfile<'info> {
         init,
         payer = authority,
         space = 0, // No data, just holds lamports
-        seeds = [b"vault", user.key().as_ref()],
+        seeds = [b"vault", profile.key().as_ref()],
         bump
     )]
     /// CHECK: Safe because it's a PDA with known seeds and only receives SOL via system program
     pub vault: UncheckedAccount<'info>,
+
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-#[instruction(liked: Pubkey)]
-pub struct CreateLike<'info> {
+#[instruction(bio: String)]
+pub struct UpdateProfile<'info> {
     #[account(mut)]
-    pub liker: Signer<'info>,
-    #[account(
-        init_if_needed,
-        seeds = [b"like_vault", liker.key().as_ref()],
-        bump,
-        payer = liker,
-        space = ANCHOR_DISCRIMINATOR_SIZE + Like::INIT_SPACE
-    )]
-    pub likes_vault: Account<'info, Like>,
+    pub authority: Signer<'info>,
+
     #[account(
         mut,
-        seeds = [b"user", user.key().as_ref()],
+        seeds = [b"profile", authority.key().as_ref(), profile.profile_name.as_bytes()],
         bump,
         has_one = authority,
     )]
-    pub liker_profile: Account<'info, User>,
-    pub system_program: Program<'info, System>,
+    pub profile: Account<'info, Profile>,
 }
 
 /**
  * STATE
  */
-
 #[account]
 #[derive(InitSpace, Debug)]
-pub struct User {
+pub struct Profile {
     pub authority: Pubkey, // Wallet controlling the user profile
-    #[max_len(USERNAME_MAX_LEN)]
-    pub username: String, // Profile name
+    #[max_len(PROFILE_NAME_MAX_LEN)]
+    pub profile_name: String, // Profile name
     #[max_len(BIO_MAX_LEN)]
     pub bio: String, // Description of charity
     #[max_len(GENDER_MAX_LEN)]
     pub gender: String,
-    #[max_len(INTERESTS_MAX_LEN)]
-    pub interests: String, // Comma-separated interests
     #[max_len(LOOKING_FOR_MAX_LEN)]
     pub looking_for: String, // Match preference
     #[max_len(AVATAR_URI_MAX_LEN)]
-    pub avatar_uri: String, // IPFS or Arweave link
+    pub avatar_uri: String, // IPFS link
     pub created_at: i64, // When profile was created
-    pub updated_at: i64,   // When profile was updated
-    pub paused: bool,      // Reject new matches
+    pub updated_at: i64, // When profile was updated
+    pub paused: bool, // Reject new matches
     pub deleted_at: Option<i64>, // When profile was deleted
-    pub vault_bump: u8,    // Reference to associated vault PDA
-}
-
-#[account]
-pub struct Like {
-    pub user: Pubkey, // The owner of this vault (user profile)
-    #[max_len(500)]
-    pub liked: Vec<Pubkey>, // List of users this person liked
-    #[max_len(500)]
-    pub liked_by: Vec<Pubkey>, // List of users who liked this person
-    pub status: LikeStatus, // Pending | Matched
-    pub timestamp: i64, // When the like happened
-}
-
-#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
-pub enum LikeStatus {
-    Pending,
-    Matched,
-}
-
-/**
- * ERROR_CODES
- */
-#[error_code]
-pub enum ErrorCode {
-    #[msg("Unauthorized access")]
-    Unauthorized
+    pub vault_bump: u8, // Reference to associated vault PDA
 }
