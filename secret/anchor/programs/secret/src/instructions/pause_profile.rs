bd59ca606b1e316ca1ab45d0eedcda74abeddbec
ulieth <uliana.eth@proton.me>
2025-05-13 17:46:35 +0200

    added pause_profile.rs instruction

---
+++ secret/anchor/programs/secret/src/instructions/pause_profile.rs
@@ -0,0 +1,18 @@
+use anchor_lang::prelude::*;
+
+use crate::state::Profile;
+
+#[derive(Accounts)]
+#[instruction(paused: bool)]
+pub struct PauseProfile<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+      mut,
+      seeds = [b"profile", authority.key().as_ref(), profile.profile_name.as_bytes()],
+      bump,
+      has_one = authority
+    )]
+    pub profile: Account<'info, Profile>
+}
